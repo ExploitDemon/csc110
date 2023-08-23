1687686d944421558243721ac7a9b2b22ee30ae7
ExploitDemon <exploitdemon@gmail.com>
2023-08-23 14:12:26 -0700

    this shit is stupid

---
+++ programming_projects/programming_project_2/poem.py
@@ -0,0 +1,9 @@
+print("When day comes, we ask ourselves,"
+      " where can we find light in this"
+      " never-ending\n\n"
+      "The Loss we carry. A sea we must wade.\n"
+      "We braved the belly of the beast.\n\n"
+      "We've learned that quiet isn't always peace,\n"
+      "and the norms and notions of what \"just\" is isn't always justice.\n\n"
+      "And yet the dawn is ours before we knew it.\n\n"
+      "Somehow we do it.")
