3e296b27a3d06e76a6f0b1cca23d168900d2a61b
ExploitDemon <exploitdemon@gmail.com>
2023-08-23 14:16:51 -0700

    a lil changes

--- programming_projects/programming_project_2/poem.py
+++ programming_projects/programming_project_2/poem.py
@@ -1,9 +1,27 @@
-print("When day comes, we ask ourselves,"
-      " where can we find light in this"
-      " never-ending\n\n"
-      "The Loss we carry. A sea we must wade.\n"
-      "We braved the belly of the beast.\n\n"
-      "We've learned that quiet isn't always peace,\n"
-      "and the norms and notions of what \"just\" is isn't always justice.\n\n"
-      "And yet the dawn is ours before we knew it.\n\n"
-      "Somehow we do it.")
+# print("When day comes, we ask ourselves,"
+#       " where can we find light in this"
+#       " never-ending\n\n"
+#       "The Loss we carry. A sea we must wade.\n"
+#       "We braved the belly of the beast.\n\n"
+#       "We've learned that quiet isn't always peace,\n"
+#       "and the norms and notions of what \"just\" is isn't always justice.\n\n"
+#       "And yet the dawn is ours before we knew it.\n\n"
+#       "Somehow we do it.")
+# poem.py
+
+def main():
+      excerpt = (
+            "When day comes, we ask ourselves, where can we find light in this never-ending shade?\n"
+            "\nThe loss we carry. A sea we must wade.\n"
+            "We braved the belly of the beast.\n"
+            "\nWe've learned that quiet isn't always peace,"
+            "and the norms and notions of what \"just\" is isn't always justice.\n"
+            "\nAnd yet the dawn is ours before we knew it.\n"
+            "\nSomehow we do it."
+      )
+
+      print(excerpt, end="")  # Print the excerpt without a newline character at the end
+
+
+if __name__ == "__main__":
+      main()
