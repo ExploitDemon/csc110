0bc63b865b7b2fc21ad41d7484c204c152b9bca6
ExploitDemon <exploitdemon@gmail.com>
2023-12-01 13:23:13 -0700

    added while condition to handle if multiple values exist in the original list

--- attendance_problems/remove_and_append.py
+++ attendance_problems/remove_and_append.py
@@ -3,5 +3,12 @@ def remove_and_append(original_list, to_remove_list, to_append_list):
     for item in to_remove_list:
         while item in original_list:
             original_list.remove(item)
-    for item in to_append_list:
-        original_list.append(item)
+    original_list.extend(to_append_list)
+    return original_list
+
+
+if __name__ == "__main__":
+    test_list = [1, 2, 3]
+    remove_and_append(test_list, [2, 3, 4], [10, 11])
+    assert test_list == [1, 10, 11]
+    print(test_list)  # prints: [1, 10, 11]
